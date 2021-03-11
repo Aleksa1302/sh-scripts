da7961bc322373b7ca1a2da0acddaaf815adcfd9
Aleksa1302 <aleksa1302@gmail.com>
2021-03-11 14:38:45 -0500

    Update cpdo.sh

--- cpdo.sh
+++ cpdo.sh
@@ -14,28 +14,22 @@ if [ $hostn == y ]
 then
 	# System Update
 	yum update
-	
 	# Change of Directory
 	cd /home
-	
 	# Download Latest Version of cPanle-DNS Only
 	curl -o latest-dnsonly -L https://securedownloads.cpanel.net/latest-dnsonly
-	
 	# Start Installation
 	sh latest-dnsonly 
-	
 	# Finish 
 	echo "Continue setup in WHM"
 	echo "http://$HOSTNAME:2086/"
-	
 	# Removing Script from System
 	rm cpdo.sh
-	
 	# Terminate our shell script with success message
 	exit 0
 	
 else
-   # hostname update part
+   # hostname update in case that hostname is not in required format ns.example.com
    echo "After hostname change we will reboot server and you will have to re run script "
    echo "Enter desired Hostname?"
    read host
